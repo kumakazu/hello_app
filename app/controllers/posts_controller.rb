00823d798e3d70f1bacff30ea9015357d87dc2b7
EC2 Default User <ec2-user@ip-172-31-16-71.us-east-2.compute.internal>
2020-07-14 06:33:02 +0000

    update作成 new.html.erbにname属性追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,4 +24,15 @@ class PostsController<ApplicationController
   def edit
     @post=Post.find_by(id: params[:id])
   end
+  
+  def update
+    @post=Post.find_by(id: params[:id])
+    @post.content=params[:content]
+    if @post.save
+      flash[:notece]="投稿を編集しました"
+      redirect_to("/posts/index")
+    else
+      render("posts/edit")
+    end
+  end
 end
