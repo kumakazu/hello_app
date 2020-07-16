ccae4a9de81da9b7d5deef5afd0668588d9b9fb1
EC2 Default User <ec2-user@ip-172-31-16-71.us-east-2.compute.internal>
2020-07-16 08:28:53 +0000

    dberrordestroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "posts/index"=>"posts#index"
   get "posts/new" => "posts#new"
